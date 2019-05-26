da55a4fcfaf72ba26e8dae63fe88105ce3d1e5a3
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2019-05-27 01:57:22 +0700

    Implement tag command
    * run merge from tag
    * create tag
    * Increase semvers
    * Use semver to sort latest versions
    * trim suffix .0
    * zeroes prefix

--- cmd/tag.go
+++ cmd/tag.go
@@ -1,6 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"os/exec"
+	"sort"
+	"strings"
+
+	"github.com/Masterminds/semver"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +27,71 @@ Assume we have last tag '1.2.3'.
 Default behavior calls 'git op merge' before releasing.`,
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		mergeCmd.Run(cmd, []string{})
+
+		version := "patch"
+		if len(args) > 0 {
+			version = args[0]
+		}
+
+		latestVersion := "0.0.0"
+		v, nil := semver.NewVersion(latestVersion)
+		out, err := exec.Command("git", "tag").CombinedOutput()
+		if err != nil {
+			log.Fatal(fmt.Sprintf("%s", out))
+		}
+		if len(out) > 0 {
+			tags := strings.Split(strings.Trim(fmt.Sprintf("%s", out), "\n"), "\n")
+			if len(tags) == 1 && tags[0] == "" {
+				tags = []string{}
+			}
+			vs := make([]*semver.Version, len(tags))
+			for i, r := range tags {
+				localV, err := semver.NewVersion(r)
+				if err != nil || !strings.Contains(r, ".") {
+					vs[i] = v
+				} else {
+					vs[i] = localV
+				}
+			}
+			sort.Sort(semver.Collection(vs))
+			v = vs[len(vs)-1]
+			latestVersion = v.Original()
+		}
+
+		if "patch" == version || "minor" == version || "major" == version {
+			if "patch" == version {
+				v := v.IncPatch()
+				version = v.String()
+			}
+			if "minor" == version {
+				v := v.IncMinor()
+				version = v.String()
+			}
+			if "major" == version {
+				v := v.IncMajor()
+				version = v.String()
+			}
+			versionParts := strings.Split(version, ".")
+			latestVersionParts := strings.Split(latestVersion, ".")
+			for i, p := range latestVersionParts {
+				if p != "0" {
+					versionParts[i] = strings.TrimRight(p, "123456789") + versionParts[i]
+				}
+			}
+			version = strings.Join(versionParts, ".")
+			version = strings.TrimSuffix(version, ".0")
+		}
+
+		out, err = exec.Command("git", "tag", version, "master").CombinedOutput()
+		if err != nil {
+			log.Fatal(fmt.Sprintf("%s", out))
+		}
+		if len(out) > 0 {
+			fmt.Printf("%s\n", out)
+		} else {
+			fmt.Printf("Tag '%s' created on branch '%s'\n", version, "master")
+		}
 	},
 }
 
